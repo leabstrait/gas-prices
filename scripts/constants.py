5e677fdbba2c6fd7e95475c47e14a383dfb39bef
Labin Ojha <leabstrait@gmail.com>
2019-12-12 00:37:12 +0545

    Separate out Constants and Utils

--- scripts/constants.py
+++ scripts/constants.py
@@ -1,11 +1,22 @@
+from os.path import join
+
 # Key for U.S. Energy Information Administration API
 EIA_API_KEY = 'a67246b03bbf71566663a8b5564f5d4c'
 
-# API Prams IDs
+# API Param's IDs
 DAILY_SERIES_ID = 'NG.RNGWHHD.D'                    # Henry Hub Natural Gas Spot Price, Daily
 WEEKLY_SERIES_ID = 'NG.RNGWHHD.W'                   # Henry Hub Natural Gas Spot Price, Weekly
 MONTHLY_SERIES_ID = 'NG.RNGWHHD.M'                  # Henry Hub Natural Gas Spot Price, Monthly
 ANNUAL_SERIES_ID = 'NG.RNGWHHD.A'                   # Henry Hub Natural Gas Spot Price, Annual
+
+# URIs for scraping
+DAILY_SERIES_URI = 'https://www.eia.gov/dnav/ng/hist/rngwhhdD.htm'      # Henry Hub Natural Gas Spot Price, Daily
+WEEKLY_SERIES_URI = 'https://www.eia.gov/dnav/ng/hist/rngwhhdW.htm'     # Henry Hub Natural Gas Spot Price, Weekly
+MONTHLY_SERIES_URI = 'https://www.eia.gov/dnav/ng/hist/rngwhhdM.htm'    # Henry Hub Natural Gas Spot Price, Monthly
+ANNUAL_SERIES_URI = 'https://www.eia.gov/dnav/ng/hist/rngwhhdA.htm'     # Henry Hub Natural Gas Spot Price, Annual
+
+# Data Directory
+DATA_DIR = join('..', 'data')
 
 # Text Matter for datapackage.json
 DATAPACKAGE_JSON = '''{
@@ -13,72 +24,108 @@ DATAPACKAGE_JSON = '''{
     "title": "Henry Hub Natural Gas Spot prices listed Daily, Weekly, Monthly and  Annually",
     "resources": [
         {
-            "name": "daily_gas_prices",
-            "path": "data/daily_gas_prices.csv",
+            "name": "api_daily_gas_prices",
+            "path": "data/api_daily_gas_prices.csv",
             "format": "csv",
             "mediatype": "text/csv",
             "schema": {
                 "fields": [
                     {
-                        "name": "Date",
+                        "name": "date",
                         "type": "date"
                     },
                     {
-                        "name": "Price(Dollars per Million Btu)",
+                        "name": "price",
                         "type": "float"
                     }
                 ]
             }
         },
         {
-            "name": "weekly_gas_prices",
-            "path": "data/weekly_gas_prices.csv",
+            "name": "api_weekly_gas_prices",
+            "path": "data/api_weekly_gas_prices.csv",
             "format": "csv",
             "mediatype": "text/csv",
             "schema": {
                 "fields": [
                     {
-                        "name": "Date",
+                        "name": "date",
                         "type": "date"
                     },
                     {
-                        "name": "Price(Dollars per Million Btu)",
+                        "name": "price",
                         "type": "float"
                     }
                 ]
             }
         },
         {
-            "name": "monthly_gas_prices",
-            "path": "data/monthly_gas_prices.csv",
+            "name": "api_monthly_gas_prices",
+            "path": "data/api_monthly_gas_prices.csv",
             "format": "csv",
             "mediatype": "text/csv",
             "schema": {
                 "fields": [
                     {
-                        "name": "Date",
+                        "name": "date",
                         "type": "date"
                     },
                     {
-                        "name": "Price(Dollars per Million Btu)",
+                        "name": "price",
                         "type": "float"
                     }
                 ]
             }
         },
         {
-            "name": "annual_gas_prices",
-            "path": "data/annual_gas_prices.csv",
+            "name": "api_annual_gas_prices",
+            "path": "data/api_annual_gas_prices.csv",
             "format": "csv",
             "mediatype": "text/csv",
             "schema": {
                 "fields": [
                     {
-                        "name": "Date",
+                        "name": "date",
                         "type": "date"
                     },
                     {
-                        "name": "Price(Dollars per Million Btu)",
+                        "name": "price",
+                        "type": "float"
+                    }
+                ]
+            }
+        },
+        {
+            "name": "scrape_daily_gas_prices",
+            "path": "data/scrape_daily_gas_prices.csv",
+            "format": "csv",
+            "mediatype": "text/csv",
+            "schema": {
+                "fields": [
+                    {
+                        "name": "date",
+                        "type": "date"
+                    },
+                    {
+                        "name": "price",
+                        "type": "float"
+                    }
+                ]
+            }
+        },
+        {
+            "name": "scrape_monthly_gas_prices",
+            "path": "data/scrape_monthly_gas_prices.csv",
+            "format": "csv",
+            "mediatype": "text/csv",
+            "schema": {
+                "fields": [
+                    {
+                        "name": "date",
+                        "type": "date"
+                    },
+                    {
+                        "name": "price",
                         "type": "float"
                     }
                 ]
---
+++ scripts/utils.py
@@ -0,0 +1,24 @@
+import csv
+from os import mkdir
+from os.path import join, exists
+
+import constants
+
+# Make the data dir if doesn't exist
+def make_data_dir():
+    if not exists(constants.DATA_DIR):
+        mkdir(constants.DATA_DIR)
+
+# Write data to the provided CSV fle
+def toCSV(dataRows, csvFile):
+    header = ['date', 'price']
+    with open(csvFile, 'w+') as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+        writer.writerows(dataRows)
+
+# Write the datapackage.json file
+def makeDatapackage():
+    f = open('../datapackage.json', 'w+')
+    f.write(constants.DATAPACKAGE_JSON)
+    f.close()
