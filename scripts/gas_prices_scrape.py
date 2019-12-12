c3455de4a6d38280e3f3b18bc9304e1743348246
Labin Ojha <leabstrait@gmail.com>
2019-12-12 12:24:03 +0545

    Add code for scraping annual prices

--- scripts/gas_prices_scrape.py
+++ scripts/gas_prices_scrape.py
@@ -92,12 +92,42 @@ def scrape_monthly_data():
         
     toCSV(records, csv_file_monthly)
 
+# ANNUAL
+
+def scrape_annual_data():
+
+    resp = urllib.request.urlopen(constants.ANNUAL_SERIES_URI)
+    resp_data = resp.read()
+
+    soup = BeautifulSoup(resp_data, 'html.parser')
+    data_table = soup.find_all('table')[5]
+
+    all_decades = data_table.find_all('td', {"class": "B4"})
+    all_prices  = data_table.find_all('td', {"class": "B3"})
+
+    records = []
+    for decade in all_decades:
+        year = 0
+        for price in all_prices:
+            decade_prefix = int(decade.string.strip()[:3])
+            date = str(decade_prefix) + str(year)
+            
+            year = year + 1
+            records.append([date, price.string])
+
+            if year > 9:
+                all_prices = all_prices[10:]
+                break
+        
+    toCSV(records, csv_file_annual)
+
 
 if __name__ == '__main__':
     scrape_daily_data()
-    #TODO: scrape_weekly_data()
+    # TODO: scrape_weekly_data()
     scrape_monthly_data()
-    #TODO: scrape_annual_data
+    scrape_annual_data()
+
     makeDatapackage()
 
 
